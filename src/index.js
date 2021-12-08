cadbda046b7863fec2496dd4a1c65366d2cb17da
Fang Yu Liin <nicolelin0531@yahoo.com.tw>
2021-12-08 18:31:45 +0800

    首頁面基礎佈局
    首頁面元件佈局
    Header 佈局
    路徑配置 jsconfig.json

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDom from "react-dom";
-import Login from "./components/Login";
-import "./css/app.scss";
-import "./css/style.scss";
+import App from "components/App";
+import "css/app.scss";
+import "css/style.scss";
 
-ReactDom.render(<Login />, document.getElementById("root"));
+ReactDom.render(<App />, document.getElementById("root"));
