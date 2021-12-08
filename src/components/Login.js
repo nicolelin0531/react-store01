bcf23e4a382f1623b6cd4a6c649d872ca8e374e2
Fang Yu Liin <nicolelin0531@yahoo.com.tw>
2021-12-08 17:49:05 +0800

    引入bulma
    install bulma
    login css調整
    login.js 調整(加上className)
    全局引用css(放index.html)

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,27 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 class Login extends React.Component {
   render() {
     return (
-      <Fragment>
-        <form className="login-wrapper">
-          <div>
-            <label>Email</label>
-            <div>
-              <input type="text" placeholder="Email" />
+      <div className="login-wrapper">
+        <form className="box login-box">
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+              <input className="input" type="text" placeholder="Email" />
             </div>
           </div>
-          <div>
-            <label>Password</label>
-            <div>
-              <input type="text" placeholder="Password" />
+          <div className="field">
+            <label className="label">Password</label>
+            <div className="control">
+              <input className="input" type="text" placeholder="Password" />
             </div>
           </div>
-          <div>
-            <button>Login</button>
+          <div className="control">
+            <button className="button is-fullwidth is-primary">Login</button>
           </div>
         </form>
-      </Fragment>
+      </div>
     ); //jsx技術
   }
 }
