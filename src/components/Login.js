45acca10190651d8e782d61b4d2e54dee0920bf3
Fang Yu Liin <nicolelin0531@yahoo.com.tw>
2021-12-08 16:24:48 +0800

    Login.js畫面基本佈局

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 class Login extends React.Component {
   render() {
-    return <p>Login Component</p>; //jsx技術 Babel
+    return (
+      <Fragment>
+        <form className="login-wrapper">
+          <div>
+            <label>Email</label>
+            <div>
+              <input type="text" placeholder="Email" />
+            </div>
+          </div>
+          <div>
+            <label>Password</label>
+            <div>
+              <input type="text" placeholder="Password" />
+            </div>
+          </div>
+          <div>
+            <button>Login</button>
+          </div>
+        </form>
+      </Fragment>
+    ); //jsx技術
   }
 }
 
