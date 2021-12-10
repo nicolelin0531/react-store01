5e923564dc0d4b880418d44bfb20ce102c6135d5
Fang Yu Liin <nicolelin0531@yahoo.com.tw>
2021-12-10 18:35:48 +0800

    Router login跳轉

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 class Login extends React.Component {
+  handleSubmit = (event) => {
+    // 1. 阻止默認事件行為
+    event.preventDefault();
+
+    // 2. 獲取表單數據
+
+    // 3. 處理登入邏輯
+
+    // 4. 跳轉到首頁
+    // this.props.history.push("/");
+    this.props.navigate("/");
+  };
+
   render() {
     return (
       <div className="login-wrapper">
-        <form className="box login-box">
+        <form className="box login-box" onSubmit={this.handleSubmit}>
           <div className="field">
             <label className="label">Email</label>
             <div className="control">
@@ -25,5 +39,9 @@ class Login extends React.Component {
     ); //jsx技術
   }
 }
+function WithNavigate(props) {
+  let navigate = useNavigate();
+  return <Login {...props} navigate={navigate} />;
+}
 
-export default Login;
+export default WithNavigate;
