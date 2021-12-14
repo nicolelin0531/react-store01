145deb4134d0734952cb4bf24405c00932bdc9ce
Fang Yu Liin <nicolelin0531@yahoo.com.tw>
2021-12-14 17:40:21 +0800

    json-server數據渲染

--- src/components/Products.js
+++ src/components/Products.js
@@ -3,64 +3,28 @@ import ToolBox from "components/ToolBox";
 import Product from "components/Product";
 
 class Products extends React.Component {
-  product = {
-    name: "我的英雄學院",
-    image: "images/2.jpg",
-    tags: "26集",
-    price: 800,
-    status: "available",
+  state = {
+    products: [],
   };
 
-  products = [
-    {
-      id: 1,
-      name: "獵人",
-      image: "images/2.jpg",
-      tags: "26集",
-      price: 800,
-      status: "available",
-    },
-    {
-      id: 2,
-      name: "進擊的巨人",
-      image: "images/3.jpg",
-      tags: "26集",
-      price: 800,
-      status: "available",
-    },
-    {
-      id: 3,
-      name: "鋼之鍊金術師",
-      image: "images/4.jpg",
-      tags: "26集",
-      price: 800,
-      status: "available",
-    },
-    {
-      id: 4,
-      name: "鬼滅之刃",
-      image: "images/5.jpg",
-      tags: "26集",
-      price: 800,
-      status: "available",
-    },
+  componentDidMount() {
+    fetch("http://localhost:3003/products")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.setState({
+          products: data,
+        });
+      });
+  }
 
-    {
-      id: 5,
-      name: "我的英雄學院",
-      image: "images/6.jpg",
-      tags: "26集",
-      price: 800,
-      status: "unavailable",
-    },
-  ];
   render() {
     return (
       <div>
         <ToolBox></ToolBox>
         <div className="products">
           <div className="columns is-multiline is-desktop">
-            {this.products.map((p) => {
+            {this.state.products.map((p) => {
               return (
                 <div className="column is-3" key={p.id}>
                   <Product product={p}></Product>
