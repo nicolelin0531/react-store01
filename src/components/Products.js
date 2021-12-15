d15fe32f61eaf130f906e4a70a13ebe926d2d174
Fang Yu Liin <nicolelin0531@yahoo.com.tw>
2021-12-15 12:47:31 +0800

    商品列表搜索功能

--- src/components/Products.js
+++ src/components/Products.js
@@ -6,20 +6,42 @@ import Product from "components/Product";
 class Products extends React.Component {
   state = {
     products: [],
+    sourceProducts: [],
   };
 
   componentDidMount() {
     axios.get("/products").then((response) => {
       this.setState({
         products: response.data,
+        sourceProducts: response.data,
       });
     });
   }
 
+  //search
+  search = (text) => {
+    console.log(text);
+    //1. Get New Array
+    let _products = [...this.state.sourceProducts];
+
+    //2. Filter New Array
+    _products = _products.filter((p) => {
+      //name: Abcd text:ab ===> ['Ab']
+      //text: '' ===> ["","","",""]
+      const matchArray = p.name.match(new RegExp(text, "gi")); //gi：全局不區分大小寫
+      return !!matchArray;
+    });
+
+    //3. set State
+    this.setState({
+      products: _products,
+    });
+  };
+
   render() {
     return (
       <div>
-        <ToolBox></ToolBox>
+        <ToolBox search={this.search}></ToolBox>
         <div className="products">
           <div className="columns is-multiline is-desktop">
             {this.state.products.map((p) => {
