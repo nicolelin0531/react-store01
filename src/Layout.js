aa0dd7fd5d8a4dfd2859497f44c5f6e828e5b3c4
Fang Yu Liin <nicolelin0531@yahoo.com.tw>
2021-12-19 23:41:37 +0800

    App, Cart 外層使用Layout模板

--- src/Layout.js
+++ src/Layout.js
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "components/Header";
 
 const Layout = (props) => {
-  <div className="main">
-    <Header />
-    {props.children}
-  </div>;
+  return (
+    <div className="main">
+      <Header />
+      {props.children}
+    </div>
+  );
 };
 
 export default Layout;
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,25 +1,26 @@
 import React from "react";
-import Header from "components/Header";
+// import Header from "components/Header";
+import Layout from "Layout";
 
-// const Cart = () => {
-//   <Layout>
-//   <div className="cart-page">
-//     <p className="title has-text-centered">Cart Page</p>
-//   </div>;
-//   </Layout>;
-// };
-
-class Cart extends React.Component {
-  render() {
-    return (
-      <div className="main">
-        <Header />
-        <div className="cart-page">
-          <p className="title has-text-centered">Cart Page</p>
-        </div>
+const Cart = () => {
+  return (
+    <Layout>
+      <div className="cart-page">
+        <p className="title has-text-centered">Cart Page</p>
       </div>
-    );
-  }
-}
+    </Layout>
+  );
+};
+
+// class Cart extends React.Component {
+//   render() {
+//     return (
+//       <div className="main">
+//         <Header />
+//         <div className="cart-page"></div>
+//       </div>
+//     );
+//   }
+// }
 
 export default Cart;
